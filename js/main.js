df7201d2c7bf3e21ac472f58170060f7bca8383c
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2024-04-18 18:29:04 +0200

    updated html and added dom variables

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
-// * DOM ELEMENTS & GLOBAL VARIABLES
+//* ELEMENTOS DE DOM Y VARIABLES GLOBALES
+
+// pantallas
+const splashScreenNode = document.querySelector("#splash-screen")
+const gameScreenNode = document.querySelector("#game-screen")
+const gameOverScreenNode = document.querySelector("#game-over-screen")
+
+// botones
+const startBtnNode = document.querySelector("#start-btn")
+
+// game box
+const gameBoxNode = document.querySelector("#game-box")
+
+
+
+//* FUNCIONES GLOBALES DE CAMBIO DE ESTADO E INICIO
 
 
 
 
 
-// * STATE MANAGEMENT FUNCTIONS
 
-
-
-
-
-
-// * EVENT LISTENERS
+//* EVENT LISTENERS
 
 
 
